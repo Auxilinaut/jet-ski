b09ab82b23292da8a3e735070df575dbfad57771
Justin Johnson <jj246@njit.edu>
2018-10-15 04:11:31 -0400

    git init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Jet Ski
+## How to run (dev)
+Open up the scene: src\unity\Assets\JetSki\Instance\Scenes\instance.unity
+## How to play
+Keyboard: WASD or arrows to move. Left click to rocket jet boost. Right click to water jet boost.
---
+++ src/unity/Assets/JetSki/Instance/Scripts/BoatAlignNormal.cs
@@ -0,0 +1,182 @@
+﻿// This file is subject to the MIT License as seen in the root of this folder structure (LICENSE)
+
+using UnityEngine;
+using Crest;
+using System.Collections.Generic;
+
+public class BoatAlignNormal : MonoBehaviour
+{
+    private struct Inputs
+    {
+        public float forward;
+        public float sideways;
+        public bool left;
+        public bool right;
+        public bool jump;
+    }
+
+    private struct InputMessage
+    {
+        public float delivery_time;
+        public uint start_tick_number;
+        public List<Inputs> inputs;
+    }
+
+    private struct ClientState
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+    }
+
+    private struct StateMessage
+    {
+        public float delivery_time;
+        public uint tick_number;
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 velocity;
+        public Vector3 angular_velocity;
+    }
+
+    public float _bottomH = -1f;
+    public bool _debugDraw = false;
+    public float _overrideProbeRadius = -1f;
+    public float _buoyancyCoeff = 40000f;
+    public float _boyancyTorque = 2f;
+
+    public float _forceHeightOffset = -1f;
+    public float _enginePower = 10000f;
+    public float _turnPower = 100f;
+
+    public float _boatWidth = 2f;
+
+    Rigidbody _rb;
+
+    public float _dragInWaterUp = 20000f;
+    public float _dragInWaterRight = 20000f;
+    public float _dragInWaterForward = 20000f;
+
+    bool _inWater;
+    public bool InWater { get { return _inWater; } }
+
+    Vector3 _velocityRelativeToWater;
+    public Vector3 VelocityRelativeToWater { get { return _velocityRelativeToWater; } }
+
+    Vector3 _displacementToBoat, _displacementToBoatLastFrame;
+    bool _displacementToBoatInitd = false;
+    public Vector3 DisplacementToBoat { get { return _displacementToBoat; } }
+    
+    private bool RocketBoosting { get; set; }
+    private bool WaterBoosting { get; set; }
+
+    public bool _playerControlled = true;
+    public float _throttleBias = 0f;
+    public float _steerBias = 0f;
+    public Vector3 RocketThrust = new Vector3(0, 0, 100000f);
+    public float WaterThrust = 15f;
+    private bool CameraLockedOnBall = false;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        var colProvider = OceanRenderer.Instance.CollisionProvider;
+        var position = transform.position;
+
+        var undispPos = Vector3.zero;
+        if (!colProvider.ComputeUndisplacedPosition(ref position, ref undispPos)) return;
+
+        if (!colProvider.SampleDisplacement(ref undispPos, ref _displacementToBoat)) return;
+        if (!_displacementToBoatInitd)
+        {
+            _displacementToBoatLastFrame = _displacementToBoat;
+            _displacementToBoatInitd = true;
+        }
+
+        // estimate water velocity
+        Vector3 velWater = (_displacementToBoat - _displacementToBoatLastFrame) / Time.deltaTime;
+        _displacementToBoatLastFrame = _displacementToBoat;
+
+        var normal = Vector3.zero;
+        if (!colProvider.SampleNormal(ref undispPos, ref normal, _boatWidth)) return;
+        Debug.DrawLine(transform.position, transform.position + 5f * normal);
+
+        _velocityRelativeToWater = _rb.velocity - velWater;
+
+        var dispPos = undispPos + _displacementToBoat;
+        float height = dispPos.y;
+
+        float bottomDepth = height - transform.position.y - _bottomH;
+
+        _inWater = bottomDepth > 0f;
+
+        float forward = _throttleBias;
+        float sideways = _steerBias;
+
+        if (_playerControlled)
+        {
+            forward += Input.GetAxis("Vertical");
+            sideways += Input.GetAxis("Horizontal");
+            RocketBoosting = Input.GetMouseButton(0) | Input.GetButton("Fire2");
+            WaterBoosting = Input.GetMouseButton(1) | Input.GetButton("Fire1"); //left click
+            CameraLockedOnBall = Input.GetKeyDown(KeyCode.Space) | Input.GetButtonDown("Jump");
+            if (RocketBoosting)
+            {
+                _rb.AddRelativeForce(RocketThrust, ForceMode.Acceleration);
+                //_rb.AddRelativeForce((Vector3.Scale(Vector3.right, RocketThrust) * (sideways * 0.5f)), ForceMode.Acceleration);
+            }
+            if (WaterBoosting)
+            {
+
+                if (forward != 0)
+                {
+                    _rb.AddRelativeForce(0, WaterThrust, 0, ForceMode.Acceleration);
+                    _rb.AddRelativeForce(0, 0, WaterThrust * forward, ForceMode.Acceleration);
+                    _rb.AddRelativeTorque(Vector3.right * WaterThrust * (forward * 0.25f), ForceMode.Acceleration);
+                    //_rb.AddRelativeForce(forwardVec3 * forward);
+                    //_rb.AddRelativeForce(new Vector3(1, 0, 1).normalized * forward);
+                }
+                else
+                {
+                    _rb.AddRelativeForce(0, WaterThrust, 0, ForceMode.Acceleration);
+                }
+
+                if (sideways != 0)
+                {
+                    _rb.AddRelativeTorque(Vector3.up * WaterThrust * (sideways * 0.25f), ForceMode.Acceleration);
+                    //_rb.AddRelativeTorque(Vector3.forward * WaterThrust * (sideways * 0.25f), ForceMode.Acceleration);
+                }
+            }
+            //if (CameraLockedOnBall) Camera.main.
+        }
+
+        if (!_inWater)
+        {
+            return;
+        }
+
+        var buoyancy = -Physics.gravity.normalized * _buoyancyCoeff * bottomDepth * bottomDepth * bottomDepth;
+        _rb.AddForce(buoyancy, ForceMode.Acceleration);
+
+
+        // apply drag relative to water
+        var forcePosition = _rb.position + _forceHeightOffset * Vector3.up;
+        _rb.AddForceAtPosition(Vector3.up * Vector3.Dot(Vector3.up, -_velocityRelativeToWater) * _dragInWaterUp, forcePosition, ForceMode.Acceleration);
+        _rb.AddForceAtPosition(transform.right * Vector3.Dot(transform.right, -_velocityRelativeToWater) * _dragInWaterRight, forcePosition, ForceMode.Acceleration);
+        _rb.AddForceAtPosition(transform.forward * Vector3.Dot(transform.forward, -_velocityRelativeToWater) * _dragInWaterForward, forcePosition, ForceMode.Acceleration);
+
+        _rb.AddForceAtPosition(transform.forward * _enginePower * forward, forcePosition, ForceMode.Acceleration);
+        _rb.AddTorque(transform.up * _turnPower * sideways, ForceMode.Acceleration);
+
+        //Debug.DrawLine(transform.position + Vector3.up * 5f, transform.position + 5f * (Vector3.up + transform.forward));
+
+        // align to normal
+        var current = transform.up;
+        var target = normal;
+        var torque = Vector3.Cross(current, target);
+        _rb.AddTorque(torque * _boyancyTorque, ForceMode.Acceleration);
+    }
+}
